e2e9ae6d73ca7c579a439e8d317534dfc5b15aef
aditya63077 <115339599+aditya63077@users.noreply.github.com>
2023-08-10 21:32:36 +0530

    Add files via upload

---
+++ src/Room.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    const appID=540367182;
+    const serverSecret = "3cbdd31036cd482cf7ce956990619115"
+    
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Aditya Sharma"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
+
